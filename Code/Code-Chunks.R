0763115ca12672424a3b0ba2f3cdce9acab0799c
peterantkowiak <peter.antkowiak@gmx.net>
2014-11-12 16:18:43 +0100

    Signed-off-by: peterantkowiak <peter.antkowiak@gmx.net>

---
+++ Code/Code-Chunks.R
@@ -0,0 +1,6 @@
+
+
+install.packages("adehabitat")
+install.packages("tkrplot")
+install.packages("hab", repos = "http://ase-research.org/R/", type = "source")
+install.packages("hab", repos = "http://ase-research.org/R/")
