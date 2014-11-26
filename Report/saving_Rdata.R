93dec713b84b4dcfac1acaffd310a335add25bdb
henrietteT <h.tripke@gmx.de>
2014-11-26 10:48:04 +0100

    saved .Rdata in the Report folder

---
+++ Report/saving_Rdata.R
@@ -0,0 +1,11 @@
+save(xmpl, file = "P:/Henriette/BestPracticeR/SSF-workflow/Report/xmpl.Rdata")
+save(xmpl.cut, file = "P:/Henriette/BestPracticeR/SSF-workflow/Report/xmpl.cut.Rdata")
+save(xmpl.steps, file = "P:/Henriette/BestPracticeR/SSF-workflow/Report/xmpl.steps.Rdata")
+save(xmpl.spdf, file = "P:/Henriette/BestPracticeR/SSF-workflow/Report/xmpl.spdf.Rdata")
+
+
+load("P:/Henriette/BestPracticeR/SSF-workflow/Report/xmpl.Rdata")
+load("P:/Henriette/BestPracticeR/SSF-workflow/Report/xmpl.spdf.Rdata")
+load("P:/Henriette/BestPracticeR/SSF-workflow/Report/xmpl.cut.Rdata")
+load("P:/Henriette/BestPracticeR/SSF-workflow/Report/xmpl.steps.Rdata")
+
